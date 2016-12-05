23e74202aa3e60bc1d6000adc7d4f0685626a21f
Donal Maher <dpjm94@live.ie>
2016-12-05 19:37:05 +0000

    Changes in code
    Remove the Category class
    Remove the CategorySpec class
    
    Inventory:
    Renamed List jobs to List inventory
    Rename jobs to inventory
    
    In search method:
    Rename CategorySpec to JobSpec
    Rename matchingCategorys to matchingJobs
    Replaced all reference (both type and variable name) to Category to Job and
      category to job in search method

--- src/Category.java
+++
@@ -1,9 +0,0 @@
-
-public class Category extends Job {
-
-    public Category(String categoryNo, double age, CategorySpec spec) {
-      super(categoryNo,age,spec);
-    }
-
-
-}
--- src/Inventory.java
+++ src/Inventory.java
@@ -7,23 +7,21 @@ import java.util.List;
  */
 public class Inventory {
 
-    private final List jobs;
+    private final List inventory;
 
     public Inventory(){
 
-        jobs = new LinkedList();
+        inventory = new LinkedList();
     }
 
-    public void addJob(String categoryNo, double age, JobSpec spec){
-        Job job = null;
-        if(spec instanceof CategorySpec){
-            job = new Category(categoryNo,age,(CategorySpec)spec);
-        }
-        jobs.add(job);
+    public void addJob(String categoryNo, double age,
+                       JobSpec spec){
+        Job job = new Job(categoryNo,age,spec);
+        inventory.add(job);
     }
 
     public Job getJob(String categoryNo){
-        for(Iterator i = jobs.iterator(); i.hasNext();){
+        for(Iterator i = inventory.iterator(); i.hasNext();){
             Job job = (Job)i.next();
             if(job.getCategoryNo().equals(categoryNo)){
                return job;
@@ -32,15 +30,15 @@ public class Inventory {
         return null;
     }
 
-    public List search(CategorySpec searchSpec){
-        List matchingCategorys = new LinkedList();
-        for(Iterator i = jobs.iterator(); i.hasNext();){
-            Category category = (Category)i.next();
-            if(category.getSpec().matches(searchSpec))
-            matchingCategorys.add(category);
+    public List search(JobSpec searchSpec){
+        List matchingJobs = new LinkedList();
+        for(Iterator i = inventory.iterator(); i.hasNext();){
+            Job job = (Job)i.next();
+            if(job.getSpec().matches(searchSpec))
+            matchingJobs.add(job);
         }
 
-        return matchingCategorys;
+        return matchingJobs;
 
     }
 }
