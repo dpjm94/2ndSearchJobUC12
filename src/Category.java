56ea84b84df0cbfa19f2c6926e2f2e84e2290315
Donal Maher <dpjm94@live.ie>
2016-12-05 00:05:33 +0000

    Refactoring of Job class
    Pulled up all fields from Category class
    Migrated parameter Category to Job >> (super)
    
    --there are problems(errors) in FindCategoryTester!--

--- src/Category.java
+++ src/Category.java
@@ -1,31 +1,9 @@
 
 public class Category extends Job {
-    private final String categoryNo;
-    private final double age;
-    private final CategorySpec spec;
-
 
     public Category(String categoryNo, double age, CategorySpec spec) {
-        this.categoryNo = categoryNo;
-        this.age = age;
-        this.spec = spec;
+      super(categoryNo,age,spec);
     }
 
 
-    public String getCategoryNo() {
-        return categoryNo;
-    }
-
-
-    public double getAge() {
-        return age;
-    }
-
-   /* public void setAge(double age) {
-        this.age = age;
-    }*/
-
-   public CategorySpec getSpec(){
-       return spec;
-   }
 }
--- src/Job.java
+++ src/Job.java
@@ -2,5 +2,30 @@
  * Created by donalmaher on 04/12/2016.
  */
 public abstract class Job {
+    private final String categoryNo;
+    private final double age;
+    private final JobSpec spec;
+
+
+    public Job(String categoryNo, double age, JobSpec spec) {
+        this.categoryNo = categoryNo;
+        this.age = age;
+        this.spec = spec;
+    }
+
+
+    public String getCategoryNo() {
+        return categoryNo;
+    }
+
+
+    public double getAge() {
+        return age;
+    }
+
+
+    public JobSpec getSpec(){
+        return spec;
+    }
 
 }
